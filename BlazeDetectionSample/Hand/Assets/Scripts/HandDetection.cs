2cbcd961c47fddd9af7b5ae49b6123ee43a9f8fd
Thaina <thainayu@gmail.com>
2025-03-30 15:59:16 +0700

    AR with Sentis

--- BlazeDetectionSample/Hand/Assets/Scripts/HandDetection.cs
+++ BlazeDetectionSample/Hand/Assets/Scripts/HandDetection.cs
@@ -1,35 +1,32 @@
 using System;
+
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
 using Unity.Mathematics;
 using Unity.Sentis;
-using UnityEngine;
 
 public class HandDetection : MonoBehaviour
 {
     public HandPreview handPreview;
-    public ImagePreview imagePreview;
-    public Texture2D imageTexture;
     public ModelAsset handDetector;
     public ModelAsset handLandmarker;
     public TextAsset anchorsCSV;
 
-    public float scoreThreshold = 0.5f;
+    public float scoreThreshold = 0.3f;
 
     const int k_NumAnchors = 2016;
     float[,] m_Anchors;
 
     const int k_NumKeypoints = 21;
-    const int detectorInputSize = 192;
-    const int landmarkerInputSize = 224;
 
-    Worker m_HandDetectorWorker;
-    Worker m_HandLandmarkerWorker;
-    Tensor<float> m_DetectorInput;
-    Tensor<float> m_LandmarkerInput;
     Awaitable m_DetectAwaitable;
 
-    float m_TextureWidth;
-    float m_TextureHeight;
+    [SerializeField]
+    ARCameraManager cameraManager = null;
 
+    public Unity.Sentis.DeviceType deviceType = Unity.Sentis.DeviceType.CPU;
     public async void Start()
     {
         m_Anchors = BlazeUtils.LoadAnchors(anchorsCSV.text, k_NumAnchors);
@@ -45,66 +42,89 @@ public class HandDetection : MonoBehaviour
         var idx_scores_boxes = BlazeUtils.ArgMaxFiltering(boxes, scores);
         handDetectorModel = graph.Compile(idx_scores_boxes.Item1, idx_scores_boxes.Item2, idx_scores_boxes.Item3);
 
-        m_HandDetectorWorker = new Worker(handDetectorModel, BackendType.GPUCompute);
+        using var m_HandDetectorWorker = new Worker(handDetectorModel, deviceType);
 
         var handLandmarkerModel = ModelLoader.Load(handLandmarker);
-        m_HandLandmarkerWorker = new Worker(handLandmarkerModel, BackendType.GPUCompute);
+        using var m_HandLandmarkerWorker = new Worker(handLandmarkerModel, deviceType);
 
-        m_DetectorInput = new Tensor<float>(new TensorShape(1, detectorInputSize, detectorInputSize, 3));
-        m_LandmarkerInput = new Tensor<float>(new TensorShape(1, landmarkerInputSize, landmarkerInputSize, 3));
+        var handDetectorModelShape = handDetectorModel.inputs[0].shape.ToTensorShape();
+        using var m_DetectorInput = new Tensor<float>(handDetectorModelShape);
+        using var m_LandmarkerInput = new Tensor<float>(handLandmarkerModel.inputs[0].shape.ToTensorShape());
 
-        while (true)
+        cameraManager.frameReceived += (args) => {
+            if(args.projectionMatrix is {} matrix)
+                Camera.main.projectionMatrix = matrix;
+        };
+
+        var imageTexture = new Texture2D(256,256,TextureFormat.RGBA32,false);
+        while(this)
         {
+            await Awaitable.NextFrameAsync();
+            if(!cameraManager.TryAcquireLatestCpuImage(out var image))
+                continue;
+
             try
             {
-                m_DetectAwaitable = Detect(imageTexture);
+                var textureSize = new Vector2Int(image.width,image.height);
+                if(new Vector2Int(imageTexture.width,imageTexture.height) != textureSize)
+                    imageTexture.Reinitialize(textureSize.x,textureSize.y);
+
+                var conversionParams = new XRCpuImage.ConversionParams(image, imageTexture.format) {
+#if !UNITY_EDITOR
+                    transformation = XRCpuImage.Transformation.MirrorX,
+                    outputDimensions = textureSize,
+#endif
+                };
+
+                image.Convert(conversionParams,imageTexture.GetRawTextureData<byte>());
+                imageTexture.Apply();
+
+                m_DetectAwaitable = Detect(imageTexture,m_HandDetectorWorker,m_DetectorInput,m_HandLandmarkerWorker,m_LandmarkerInput);
                 await m_DetectAwaitable;
             }
             catch (OperationCanceledException)
             {
                 break;
             }
+            finally
+            {
+                image.Dispose();
+            }
         }
-
-        m_HandDetectorWorker.Dispose();
-        m_HandLandmarkerWorker.Dispose();
-        m_DetectorInput.Dispose();
-        m_LandmarkerInput.Dispose();
     }
 
-    Vector3 ImageToWorld(Vector2 position)
+    async Awaitable Detect(Texture texture,Worker m_HandDetectorWorker,Tensor<float> m_DetectorInput,Worker m_HandLandmarkerWorker,Tensor<float> m_LandmarkerInput)
     {
-        return (position - 0.5f * new Vector2(m_TextureWidth, m_TextureHeight)) / m_TextureHeight;
-    }
+        int detectorInputSize = m_DetectorInput.shape[1];
+        int landmarkerInputSize = m_LandmarkerInput.shape[1];
 
-    async Awaitable Detect(Texture texture)
-    {
-        m_TextureWidth = texture.width;
-        m_TextureHeight = texture.height;
-        imagePreview.SetTexture(texture);
+        var textureSize = new float2(texture.width, texture.height);
 
         var size = Mathf.Max(texture.width, texture.height);
 
         // The affine transformation matrix to go from tensor coordinates to image coordinates
         var scale = size / (float)detectorInputSize;
-        var M = BlazeUtils.mul(BlazeUtils.TranslationMatrix(0.5f * (new Vector2(texture.width, texture.height) + new Vector2(-size, size))), BlazeUtils.ScaleMatrix(new Vector2(scale, -scale)));
+        var M = BlazeUtils.mul(BlazeUtils.TranslationMatrix(0.5f * (textureSize + new float2(-size, size))), BlazeUtils.ScaleMatrix(new Vector2(scale, -scale)));
         BlazeUtils.SampleImageAffine(texture, m_DetectorInput, M);
 
         m_HandDetectorWorker.Schedule(m_DetectorInput);
 
-        var outputIdxAwaitable = (m_HandDetectorWorker.PeekOutput(0) as Tensor<int>).ReadbackAndCloneAsync();
         var outputScoreAwaitable = (m_HandDetectorWorker.PeekOutput(1) as Tensor<float>).ReadbackAndCloneAsync();
-        var outputBoxAwaitable = (m_HandDetectorWorker.PeekOutput(2) as Tensor<float>).ReadbackAndCloneAsync();
+        var awaiter = outputScoreAwaitable.GetAwaiter();
+        while(!awaiter.IsCompleted)
+            await Awaitable.NextFrameAsync();
 
-        using var outputIdx = await outputIdxAwaitable;
-        using var outputScore = await outputScoreAwaitable;
-        using var outputBox = await outputBoxAwaitable;
-
-        var scorePassesThreshold = outputScore[0] >= scoreThreshold;
+        using var outputScore = awaiter.GetResult();
+        bool scorePassesThreshold = outputScore[0] >= scoreThreshold;
         handPreview.SetActive(scorePassesThreshold);
 
         if (!scorePassesThreshold)
             return;
+
+        var outputIdxAwaitable = (m_HandDetectorWorker.PeekOutput(0) as Tensor<int>).ReadbackAndCloneAsync();
+        var outputBoxAwaitable = (m_HandDetectorWorker.PeekOutput(2) as Tensor<float>).ReadbackAndCloneAsync();
+        using var outputIdx = await outputIdxAwaitable;
+        using var outputBox = await outputBoxAwaitable;
 
         var idx = outputIdx[0];
 
@@ -113,13 +133,12 @@ public class HandDetection : MonoBehaviour
         var boxCentre_TensorSpace = anchorPosition + new float2(outputBox[0, 0, 0], outputBox[0, 0, 1]);
         var boxSize_TensorSpace = math.max(outputBox[0, 0, 2], outputBox[0, 0, 3]);
 
-        var kp0_TensorSpace = anchorPosition + new float2(outputBox[0, 0, 4 + 2 * 0 + 0], outputBox[0, 0, 4 + 2 * 0 + 1]);
-        var kp2_TensorSpace = anchorPosition + new float2(outputBox[0, 0, 4 + 2 * 2 + 0], outputBox[0, 0, 4 + 2 * 2 + 1]);
+        var kp0_TensorSpace = new float2(outputBox[0, 0, 4], outputBox[0, 0, 5]);
+        var kp2_TensorSpace = new float2(outputBox[0, 0, 8], outputBox[0, 0, 9]);
         var delta_TensorSpace = kp2_TensorSpace - kp0_TensorSpace;
-        var up_TensorSpace = delta_TensorSpace / math.length(delta_TensorSpace);
         var theta = math.atan2(delta_TensorSpace.y, delta_TensorSpace.x);
         var rotation = 0.5f * Mathf.PI - theta;
-        boxCentre_TensorSpace += 0.5f * boxSize_TensorSpace * up_TensorSpace;
+        boxCentre_TensorSpace += 0.5f * boxSize_TensorSpace * math.normalizesafe(delta_TensorSpace);
         boxSize_TensorSpace *= 2.6f;
 
         var origin2 = new float2(0.5f * landmarkerInputSize, 0.5f * landmarkerInputSize);
@@ -129,20 +148,25 @@ public class HandDetection : MonoBehaviour
 
         m_HandLandmarkerWorker.Schedule(m_LandmarkerInput);
 
-        var landmarksAwaitable = (m_HandLandmarkerWorker.PeekOutput("Identity") as Tensor<float>).ReadbackAndCloneAsync();
-        using var landmarks = await landmarksAwaitable;
+        using var landmarks = await (m_HandLandmarkerWorker.PeekOutput("Identity") as Tensor<float>).ReadbackAndCloneAsync();
 
         for (var i = 0; i < k_NumKeypoints; i++)
         {
             var position_ImageSpace = BlazeUtils.mul(M2, new float2(landmarks[3 * i + 0], landmarks[3 * i + 1]));
 
-            Vector3 position_WorldSpace = ImageToWorld(position_ImageSpace) + new Vector3(0, 0, landmarks[3 * i + 2] / m_TextureHeight);
+            var position_WorldSpace = new float3(position_ImageSpace - (0.5f * textureSize),landmarks[3 * i + 2]) / textureSize.y;
+
+#if UNITY_EDITOR
+            position_WorldSpace = Camera.main.transform.TransformPoint(position_WorldSpace + new float3(0,0,1));
+#else
+            position_WorldSpace = Camera.main.transform.TransformPoint(math.mul(quaternion.Euler(0,0,-math.PI * 0.5f),position_WorldSpace) + new float3(0,0,1));
+#endif
             handPreview.SetKeypoint(i, true, position_WorldSpace);
         }
     }
 
     void OnDestroy()
     {
-        m_DetectAwaitable.Cancel();
+        m_DetectAwaitable?.Cancel();
     }
 }
